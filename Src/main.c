8434dae672742f0252921091a652479a59f49428
chipyards <noullet@chipyards.com>
2020-04-09 23:40:50 +0200

    cos angle illimite, angle en q9

--- Src/main.c
+++ Src/main.c
@@ -1,20 +1,24 @@
 // projet sin HD
 
-int cos_2pi( int a );
+int cos_q9( int a );
 
 
 int main(void)
 {
-int vtest[8];
+int vtest[10];
 
-vtest[0] = cos_2pi( 49 );
-vtest[1] = cos_2pi( 128 );
-vtest[2] = cos_2pi( 12 );
-vtest[3] = cos_2pi( 244 ); // 244 = 256-12
-vtest[4] = cos_2pi( 256 );
-vtest[5] = cos_2pi( 268 );  // 268 = 256+12
-vtest[6] = cos_2pi( 500 );  // 500 = 512-12
-vtest[7] = cos_2pi( 512 );
+// angles negatifs
+vtest[0] = cos_q9( -49 );
+vtest[1] = cos_q9( -128 );
+vtest[2] = cos_q9( -12 );
+vtest[3] = cos_q9( -244 ); // 244 = 256-12
+vtest[4] = cos_q9( -256 );
+vtest[5] = cos_q9( -268 );  // 268 = 256+12
+vtest[6] = cos_q9( -512 );
+// angles > 2pi
+vtest[7] = cos_q9( 512+49 );
+vtest[8] = cos_q9( 1024+268 );
+vtest[9] = cos_q9( 65536+12 );
 
 while (1) {}
 }
